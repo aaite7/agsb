ecb64a1d292f65376ede1a03f0bd90c17047ff72
aaite7 <choooiox@gmail.com>
2025-06-06 14:44:19 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -34,7 +34,7 @@ CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的�
 USER_NAME = "jd"         # 用户名
 UUID = "53469a40-12e6-48fa-b560-cd15782f4e50"                     # UUID，留空则自动生成
 PORT = 30123                   # Vmess端口，留空或0则自动生成
-DOMAIN = "https://sds.cyll.nyc.mn/"                   # 域名，留空则自动获取
+DOMAIN = "sds.cyll.nyc.mn"                   # 域名，留空则自动获取
 CF_TOKEN = "eyJhIjoiZGQwOTdiZGViZGM4ZGFlODdhODY4ODk4NDAzNGRiMGIiLCJ0IjoiMmZkYTQ2MTktZDk3Yy00Yjk0LWE3YWYtNjVmNGU1NDljZDMyIiwicyI6IlkyVm1aakpqT0RndE1XVm1NaTAwTWpNMUxUaGhPVFl0TXpFMlpHSmhOVFl5TW1JeSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
